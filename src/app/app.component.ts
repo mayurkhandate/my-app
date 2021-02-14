84f9b9f9ca8e44faaa4e76010b4a7f35145a4e6c
mayur khandate <mayurkhandate789.com>
2021-02-14 16:30:00 +0530

    new form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  fname = '';
+  lname = '';
+  email = '';
+  psw = '';
+  age = 0;
+  address = '';
+  city = '';
+  state = '';
+  zip = 0;
+  check = '';
+
+  formDetails: {
+    fname: string,
+    lname: string,
+    email: string,
+    psw: string,
+    age: number,
+    address: string,
+    city: string,
+    state: string,
+    zip: number,
+    check: string
+  }[] = [];
+
+  submit() {
+    const addDetails = {
+      fname: this.fname,
+      lname: this.lname,
+      email: this.email,
+      psw: this.psw,
+      age: this.age,
+      address: this.address,
+      city: this.city,
+      state: this.state,
+      zip: this.zip,
+      check: this.check
+    }
+
+    this.formDetails.push(addDetails);
+  }
 }
